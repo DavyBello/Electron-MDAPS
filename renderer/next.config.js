34853cff95fc5c7c72d36c1c35b5bfd92fcc959e
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-01-18 11:01:36 -0800

    Added export paths for pages

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -3,5 +3,11 @@ exports.webpack = config => Object.assign(config, {
 })
 
 exports.exportPathMap = () => ({
-  '/start': { page: '/start' }
+  '/start': { page: '/start' },
+  '/diagnosis': { page: '/diagnosis' },
+  '/homepage': { page: '/homepage' },
+  '/disease-records': { page: '/disease-records' },
+  '/patient-records': { page: '/patient-records' },
+  '/new-disease': { page: '/new-disease' },
+  '/new-patient': { page: '/new-patient' }
 })
